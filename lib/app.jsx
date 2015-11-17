9a374379b39f8a1f4b40ae13cb463462b851b1ed
Pierre Comalada <pierre.comalada@gmail.com>
2015-11-17 15:07:05 +0100

    commit step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -7,20 +7,38 @@ var Style = {
 };
 
 
+
 var Box = React.createClass({
   /**
    * Render a HTML button
    * @return {ReactElement}
    */
-   
+
    getInitialState: function() {
      return {value : this.props.initialValue};
    },
 
+
+
+   componentWillMount: function(){
+   	var old_this = this;
+   	this.timer = setInterval(function(){
+   	var oldValue = old_this.state.value;
+   	var newValue = oldValue === 'X' ? 'O': 'X';
+   	old_this.setState({
+   	value: newValue
+   	});
+   	}, 300);
+   	},
+
+   	componentWillUnmount: function(){
+   	clearInterval(this.timer);
+   	},
+
+
   'render': function onRender () {
     return (
       <button style={Style}>{this.state.value}</button>
-
     );
   }
 });
